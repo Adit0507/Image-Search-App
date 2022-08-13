8c0cc8d9d99294808b88acb9da0a457f220ad5e7
Adit0507 <adi05072002@gmail.com>
2022-08-13 20:45:26 +0530

    Done with the setup

---
+++ src/components/Jumbutron.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Jumbutron = () => {
+  return (
+    <div className='bg-gray-900 flex items-center py-10'>
+        <div className='max-w-md mx-auto'>
+        
+        </div>      
+    </div>
+    )
+}
+
+export default Jumbutron
