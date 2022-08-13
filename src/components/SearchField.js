7a00c4e0c792082f22d3b0064334654fe9bcaf84
Adit0507 <adi05072002@gmail.com>
2022-08-13 21:02:34 +0530

    Finished the Search Field component

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const SearchField = () => {
   return (
-    <div>SearchField</div>
-  )
-}
+    <div className="flex">
+      <input
+        className="bg-gray-50 border border-gray-300 text-sm w-full indent-2 p-2.5 outline-none focus:border-blue-500 focus:ring-2 rounded-tl rounded-bl"
+        type="search"
+        placeholder="Search any image...."
+      />
+      <button className="bg-blue-600 px-6 py-2.5 text-white rounded-tr rounded-br focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400 ">
+        Search
+      </button>
+    </div>
+  );
+};
 
-export default SearchField
+export default SearchField;
