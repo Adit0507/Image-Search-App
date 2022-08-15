5656cf774a90eacd124dd2fc22eb9a4fb020b65c
Adit0507 <adi05072002@gmail.com>
2022-08-15 17:58:53 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Jumbutron from './components/Jumbutron';
 import SearchField from './components/SearchField';
+import Images from './components/Images';
+import useAxios from './hooks/useAxios';
 
 function App() {
+  const { response, isLoading, error, fetchData } = useAxios(`search/photos?page=1&query=cats&client_id=${process.env.REACT_APP_ACCESS_KEY}`);
+  console.log(response)
   return (
     <>
       <Jumbutron > 
         <SearchField />
       </Jumbutron>
+      <Images />
     </>
   );
 }
---
+++ src/components/Images.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Images = () => {
+  return (
+    <>
+      <h1 className="text-center mt-6 underline text-2xl">Results for Cats</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
+        <h1>1</h1>
+        <h1>1</h1>
+        <h1>1</h1>
+        <h1>1</h1>
+        <h1>1</h1>
+      </div>
+    </>
+  );
+};
+
+export default Images;
