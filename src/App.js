d1292113f3913b8f68b3f6f066fe2c4389e989fb
Adit0507 <adi05072002@gmail.com>
2022-08-15 21:41:32 +0530

    Finished

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import Jumbutron from './components/Jumbutron';
 import SearchField from './components/SearchField';
 import Images from './components/Images';
 import useAxios from './hooks/useAxios';
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 // Creating Context
 export const ImageContext = createContext();
 
 function App() {
+  const [searchImage, setSearchImage] = useState();
   const { response, isLoading, error, fetchData } = useAxios(`search/photos?page=1&query=cats&client_id=${process.env.REACT_APP_ACCESS_KEY}`);
 
   const value = {
     response,
     isLoading,
     error,
-    fetchData
+    fetchData,
+    searchImage,
+    setSearchImage
   }
 
   return (
--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,18 +1,45 @@
-import React, {useState, useContext} from "react";
+import { useContext, useState } from "react";
 import { ImageContext } from "../App";
 
 const SearchField = () => {
   const [searchValue, setSearchValue] = useState("");
-  const { fetchData, setSearchImage} = useContext(ImageContext);
+  const { fetchData, setSearchImage } = useContext(ImageContext);
+
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+  const handleButtonSearch = () => {
+    fetchData(
+      `search/photos?page=1&query=${searchValue}&client_id=${process.env.REACT_APP_ACCESS_KEY}`
+    );
+    setSearchValue("");
+    setSearchImage(searchValue);
+  };
+  const handleEnterSearch = (e) => {
+    if (e.key === "Enter") {
+      fetchData(
+        `search/photos?page=1&query=${searchValue}&client_id=${process.env.REACT_APP_ACCESS_KEY}`
+      );
+      setSearchValue("");
+      setSearchImage(searchValue);
+    }
+  };
 
   return (
     <div className="flex">
       <input
         className="bg-gray-50 border border-gray-300 text-sm w-full indent-2 p-2.5 outline-none focus:border-blue-500 focus:ring-2 rounded-tl rounded-bl"
         type="search"
-        placeholder="Search any image...."
+        placeholder="Search Anything..."
+        value={searchValue}
+        onChange={handleInputChange}
+        onKeyDown={handleEnterSearch}
       />
-      <button className="bg-blue-600 px-6 py-2.5 text-white rounded-tr rounded-br focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400 ">
+      <button
+        onClick={handleButtonSearch}
+        disabled={!searchValue}
+        className="bg-blue-600 px-6 py-2.5 text-white rounded-tr rounded-br focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400"
+      >
         Search
       </button>
     </div>
